c98ee96faccf5bb834dee8efc0dbfcdb0638088b
Sébastien Petit <s.petit@compethance.fr>
2020-12-05 15:21:31 +0100

    introduction de vus js

---
+++ seb.js
@@ -0,0 +1,70 @@
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue ! ta réussi grand'
+    }
+  })
+
+  var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'Vous avez affiché cette page le ' + new Date().toLocaleString()
+    }
+  })
+
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  })
+
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { seb: 'Apprendre JavaScript' },
+        { seb: 'Apprendre Vue' },
+        { seb: 'Créer quelque chose de génial' }
+      ]
+    }
+  })
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js !'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  })
+
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: ''
+    }
+  })
+
+
+
+
+  Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }} {{ todo.id * 9}}</li>'
+  })
+  
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Légumes' },
+        { id: 1, text: 'Fromage' },
+        { id: 2, text: 'Tout ce que les humains sont supposés manger' }
+      ]
+    }
+  })
